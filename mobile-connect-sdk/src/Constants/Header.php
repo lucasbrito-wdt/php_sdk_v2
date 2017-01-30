ad8527a8b7b9b60e22b3084daea5ca99e512d82b
nrusak <n.rusak@a1qa.com>
2017-01-30 14:35:08 +0300

    Fixed WWW-Authenticate header name

--- mobile-connect-sdk/src/Constants/Header.php
+++ mobile-connect-sdk/src/Constants/Header.php
@@ -48,6 +48,6 @@ class Header
     /**
      * Key for WWW-Authenticate header
      */
-    const WWW_AUTHENTICATE = "WWW-Authenticate";
+    const WWW_AUTHENTICATE = "WWW-Authenticate-Bearer";
 }
 
